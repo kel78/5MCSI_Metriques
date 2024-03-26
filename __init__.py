666a3441bf7bf060a365cda1a1633139eccafa2f
kel78 <83736176+kel78@users.noreply.github.com>
2024-03-26 10:28:31 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
+
+
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
